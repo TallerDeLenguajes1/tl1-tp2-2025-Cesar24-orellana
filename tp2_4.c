3e1930c6a4e13b7c5819bbca978875bf2fe986ef
Cesar Orellana <cesarlorellana184@gmail.com>
2025-04-18 00:07:28 -0300

    crear_struct

---
+++ tp2_4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct 
+{
+    int velocidad;       //Velocidad del procesador en GHz (valor entre 1 y 3)
+    int anio;             //Año de fabricacion (valor entre 2015 y 2024)
+    int cantidad_nucleos;  //Cantidad de nucleos (valor entre 1 y 8)
+    char *tipo_cpu;      //Tipo de procesador(apuntador a cadena de caracteres)
+}compu;
+
+int main(){
+
+    return 0;
+}
