ad9e3881473c5e3176b27299b033a6d2e2fbd1e9
Cesar Orellana <cesarlorellana184@gmail.com>
2025-04-19 17:20:21 -0300

    Generar-PCs-2.0

--- tp2_4.c
+++ tp2_4.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 
-typedef struct compu 
+typedef struct
 {
     int velocidad;       //Velocidad del procesador en GHz (valor entre 1 y 3)
     int anio;             //Año de fabricacion (valor entre 2015 y 2024)
@@ -10,29 +10,35 @@ typedef struct compu
     char *tipo_cpu;      //Tipo de procesador(apuntador a cadena de caracteres)
 }Tcompu;
 
-Tcompu *GenerarPCs(Tcompu *pc, int cantidad);
+Tcompu *GenerarPCs(Tcompu pc[], int cantidad);
 char TipodeProcesador(int *tipo);
 
 int main(){
-    Tcompu *pcs;
-    pcs = (Tcompu*)malloc(5*sizeof(Tcompu) +1);
+    Tcompu pcs[5];
     int cantidad = 5;
-    GenerarPCs(&pcs, cantidad);
+    GenerarPCs(pcs, cantidad);
 
     free(pcs);
     return 0;
 }
 
-Tcompu *GenerarPCs(Tcompu *pc, int cant) {
-    pc->velocidad = 1 + rand() %3;
-    pc->anio = 2015 + rand() %2024;
-    pc->cantidad_nucleos = 1 + rand() %8;
-    int tipo;
-    char Atipo[10] = TipodeProcesador(&tipo);
-    pc->tipo_cpu = (Tcompu*)malloc(tipo*sizeof+1);
-    strcpy(pc->tipo_cpu,Atipo);
+Tcompu *GenerarPCs(Tcompu pc[], int cant) {
+    for (int i = 0; i < cant; i++)
+    {
+        pc[i].velocidad = 1 + rand() %3;
+        pc[i].anio = 2015 + rand() %2024;
+        pc[i].cantidad_nucleos = 1 + rand() %8;
+        int tipo;
+        char tipos[10] = TipodeProcesador(&tipo);
+        pc[i].tipo_cpu = (Tcompu *)malloc(tipo*sizeof(Tcompu) +1);
+        strcpy(pc[i].tipo_cpu , tipos);
+    }
+    
 }
 
 char TipodeProcesador(int *tipo){
-
+    char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core",
+        "Pentium"};
+    tipo = 0 + rand()% 5;
+    return tipos[*tipo];
 }
