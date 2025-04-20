19f8a4c4a8bc2a07aa1ee44d1519d5b88b43cb64
Cesar Orellana <cesarlorellana184@gmail.com>
2025-04-19 21:03:39 -0300

    funcion-Lista_PCs

--- tp2_4.c
+++ tp2_4.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 
 typedef struct
 {
@@ -11,34 +12,65 @@ typedef struct
 }Tcompu;
 
 Tcompu *GenerarPCs(Tcompu pc[], int cantidad);
-char TipodeProcesador(int *tipo);
+void TipodeProcesador(char *Atipo, int *tipo);
+void ListarPCs(Tcompu pc[], int cant);
+void LiberarMemoria(Tcompu pc[], int cant);
 
 int main(){
+    srand(time(NULL));
     Tcompu pcs[5];
     int cantidad = 5;
     GenerarPCs(pcs, cantidad);
+    ListarPCs(pcs, cantidad);
 
-    free(pcs);
+    LiberarMemoria(pcs, cantidad);
     return 0;
 }
 
 Tcompu *GenerarPCs(Tcompu pc[], int cant) {
+    int n = 10;
     for (int i = 0; i < cant; i++)
     {
         pc[i].velocidad = 1 + rand() %3;
-        pc[i].anio = 2015 + rand() %2024;
+        pc[i].anio = 2015 + rand() %10; // Para un rango entre 2015 a 2024
         pc[i].cantidad_nucleos = 1 + rand() %8;
         int tipo;
-        char tipos[10] = TipodeProcesador(&tipo);
-        pc[i].tipo_cpu = (Tcompu *)malloc(tipo*sizeof(Tcompu) +1);
-        strcpy(pc[i].tipo_cpu , tipos);
+        char Atipos[n];    // Arreglo de tipo
+        TipodeProcesador(Atipos, &tipo);
+        pc[i].tipo_cpu = (char *)malloc(strlen(Atipos)*sizeof(char) +1); // En los parentesis coloque char por que Tcompu, me daba una advertencia, y demoraba mas de compilar
+        strcpy(pc[i].tipo_cpu , Atipos);
+    }
+    return pc;
+}
+
+void TipodeProcesador(char *Atipo, int *tipo){
+    char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core",
+        "Pentium"};
+    *tipo = rand()% 6;
+    strcpy(Atipo, tipos[*tipo]);
+}
+
+void ListarPCs(Tcompu pc[], int cant){
+    printf("Lista de PCs:\n");
+
+    for (int i = 0; i < cant; i++)
+    {
+        printf("PC Numero:%d\n",(i+1));
+        printf("Velocidad del procesador: %d\n", pc[i].velocidad);
+        printf("Anio de fabricacion: %d\n", pc[i].anio);
+        printf("Cantidad de nucleos del procesador: %d\n",pc[i].cantidad_nucleos);
+        printf("Tipo de procesador: %s\n", pc[i].tipo_cpu);
+        printf("\n");
     }
     
 }
 
-char TipodeProcesador(int *tipo){
-    char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core",
-        "Pentium"};
-    tipo = 0 + rand()% 5;
-    return tipos[*tipo];
+
+
+void LiberarMemoria(Tcompu pc[], int cant){
+    for (int i = 0; i < cant; i++)
+    {
+        free(pc[i].tipo_cpu);
+    }
+    
 }
